b38f5bfca8b940a5f9011595b39c271035d0c57a
umer112 <umerfarooq889k@gmail.com>
2022-10-27 17:47:18 +0500

    bubble sort added

--- index.js
+++ index.js
@@ -32,10 +32,33 @@
 // // console.log(a);
 
 function bubbleSort(arr) {
-  for (let i = arr.length - 1; i > 0; i++) {
-    console.log(arr[i]);
-    return;
+  for (let i = arr.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+    // return;
   }
+  return arr;
 }
 const arr = [5, 10, 1, 9, 6, 29, 15];
-bubbleSort(arr);
+console.log(bubbleSort(arr), "is bubble sort");
+
+// function bubbleSort(arr) {
+//   for (let i = arr.length - 1; i > 0; i--) {
+//     for (let j = 0; j < i; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         // SWAP
+//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// Short way of  (Destructuring Assignment Array Matching)):
+
+// let a = 5, b = 6;
+// [a, b] = [b, a];
+// console.log(`${a} ${b}`);
